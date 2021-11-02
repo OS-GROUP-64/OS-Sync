6539f3376190d0804b989c401ba0ce146c79fc38
gutpdn <gut.pudinan@gmail.com>
2021-11-02 21:58:17 +0700

    completed

--- OS_Sync_Ex_01.cs
+++ OS_Sync_Ex_01.cs
@@ -9,30 +9,42 @@ namespace OS_Sync_Ex_01
         private static string x = "";
         private static int exitflag = 0;
         private static int updateFlag = 0;
+        private static Object _Lock = new Object();
 
         static void ThReadX(object i)
         {
+            
             while (exitflag == 0)
+            lock(_Lock)
             {
-                if (x != "exit")
+                if(updateFlag == 0)
                 {
-                    Console.WriteLine("***Thread {0} : x = {1}***", i, x);
+                    if (x != "exit")
+                    {
+                        Console.WriteLine("***Thread {0} : x = {1}***", i, x);
+                    }
                 }
+                updateFlag = 1;
             }
             Console.WriteLine("---Thread {0} exit---", i);
+                
         }
 
         static void ThWriteX()
         {
+            
             string xx;
             while (exitflag == 0)
+            lock(_Lock)
             {
                 Console.Write("Input: ");
                 xx = Console.ReadLine();
                 if (xx == "exit")
                     exitflag = 1;
                 x = xx;
+                updateFlag = 0;
             }
+                
         }
 
         static void Main(string[] args)
@@ -43,9 +55,9 @@ namespace OS_Sync_Ex_01
             Thread D = new Thread(ThReadX);
 
             A.Start();
-            B.Start();
-            C.Start();
-            D.Start();
+            B.Start(1);
+            C.Start(2);
+            D.Start(3);
         }
     }
 }
